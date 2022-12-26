667ea434db02ce4f6fbee9cc5d1bd8bb66400141
Emad <emadhany2007@gmail.com>
2022-12-26 18:31:37 +0400

    Initial Commit

---
+++ SwiftUI Standford/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Standford
+//
+//  Created by Emad Hany Isaac on 08/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View{
+ 
+        Text("hello My World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ SwiftUI Standford/SwiftUI_StandfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StandfordApp.swift
+//  SwiftUI Standford
+//
+//  Created by Emad Hany Isaac on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StandfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
