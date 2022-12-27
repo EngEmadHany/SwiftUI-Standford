043580ee19c246b279985f8799f4e53c72a2eb38
Emad <emadhany2007@gmail.com>
2022-12-27 11:51:07 +0400

    created cardView with two Rounded Rectangle in zstack one with fill and other with stroke
    .preferredColorScheme(.light) / .dark for preview in light and dark mode

--- SwiftUI Standford/ContentView.swift
+++ SwiftUI Standford/ContentView.swift
@@ -10,15 +10,42 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View{
- 
-        Text("hello My World")
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+            
+        }.padding(.horizontal)
+         .foregroundColor(.red)
     }
 }
+
+
+
+
+
+struct CardView: View {
+    var body: some View {
+        ZStack(){
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+                
+            Text("✈️")
+        }
+    }
+}
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
-
-
